7b2aa98230c5a64ec6d4fa2cabba1388805f21e9
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2021-01-21 12:26:04 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,7 +1,7 @@
 // Code EyesOnMe Component Here
 import React from "react";
 
-class EyesOnMe extends React.Component 
+class EyesOnMe extends React.Component
   {
     onFocus = () => console.log("NICEEE");
 
