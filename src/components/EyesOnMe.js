9f61e9d3e31e64488a02500b84db5286bcd897b6
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2021-01-21 12:25:58 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+class EyesOnMe extends React.Component 
+  {
+    onFocus = () => console.log("NICEEE");
+
+    onBlur = ()  => console.log("HEYY LOOK HERE!!")
+
+    render(){
+      return(
+        <button onFocus={this.onFocus} onBlur={this.onBlur}>
+            WHERE YOUR LOOKING SIR !?
+        </button>
+      )
+    }
+  }
+
+  export default EyesOnMe;
